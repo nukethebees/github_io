624dd4205d0d1d0bc0b2db02524bd5f28309a16c
Matthew Love <matthewlove94@gmail.com>
2025-05-13 20:31:48 +0100

    Improved stack allocation

--- hello_world_win_asm/hello_world_win_asm/main.asm
+++ hello_world_win_asm/hello_world_win_asm/main.asm
@@ -11,21 +11,19 @@ HELLO_MSG_LEN = sizeof HELLO_MSG
 
 .code
 main PROC
-    sub rsp, 28h                  ; Reserve shadow space
+    sub rsp, 28h                  ; Allocate shadow space for function calls
 
-    mov rcx, STD_OUTPUT_HANDLE    ; 
+    mov rcx, STD_OUTPUT_HANDLE    ; Set up the first argument for GetStdHandle
     call GetStdHandle             ; Get the handle to the console.
-                                  ;
+    
     mov rcx, rax                  ; Store the handle in rcx
     lea rdx, HELLO_MSG            ; Load address of the message
     mov r8, HELLO_MSG_LEN         ; Num bytes to write
-    mov r9, rsp                   ; Pointer to the number of bytes written
-    
-    sub rsp, 20h                  ; Allocate shadow space 
+    mov r9, 0                     ; nullptr for the number of bytes written
     call WriteConsoleA            ; Call Windows API to print the message
-    add rsp, 20h                  ; Clean up stack
-
-    mov rcx, 0
+    
+    add rsp, 28h                  ; Clean up stack
+    mov rcx, 0                    ; Return zero
     call ExitProcess
 main ENDP
 END
